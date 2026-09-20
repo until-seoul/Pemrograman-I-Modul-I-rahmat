dc58705ad6542f55735f6601f2bfd1ff26b60efa
until-seoul <197721271+until-seoul@users.noreply.github.com>
2026-09-20 16:39:41 +0800

    modified:   soal9/PRAK109-2610817310007-Rahmat_Setiadi.py

--- soal9/PRAK109-2610817310007-Rahmat_Setiadi.py
+++ soal9/PRAK109-2610817310007-Rahmat_Setiadi.py
@@ -1,10 +1,8 @@
-import math
-
 total_enemy = 958730
 total_heroes = 5
-enemy_per_hero = total_enemy / total_heroes
+enemy_per_hero = total_enemy // total_heroes
 print(f"Jumlah pasukan yang dibawa Yu Zhong = {total_enemy}")
 print(f"Jumlah pahlawan = {total_heroes}")
 print(
-    f"Jumlah pasukan yang harus dikalahkan setiap pahlawan adalah {math.floor(enemy_per_hero)} pasukan"
+    f"Jumlah pasukan yang harus dikalahkan setiap pahlawan adalah {enemy_per_hero} pasukan"
 )
